fe98d6cac810c555b1bd786dd9f16ef9a923aa3b
Philippe Leybaert <philippe@activa.be>
2011-04-05 20:57:26 +0200

    Split AssemblyInfo.cs

--- drivers/sqlite/Properties/AssemblyInfo.cs
+++ drivers/sqlite/Properties/AssemblyInfo.cs
@@ -22,14 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("afc2b650-415f-451b-b888-8ab8ae9955ab")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+
